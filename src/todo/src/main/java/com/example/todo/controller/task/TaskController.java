86f3528c3d6e556b34b913f748bab62a9064c078
abc-user0504 <acchankonabe@gmail.com>
2025-11-09 01:38:48 +0900

    add controller

---
+++ src/todo/src/main/java/com/example/todo/controller/task/TaskController.java
@@ -0,0 +1,30 @@
+package com.example.todo.controller.task;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@Controller
+public class TaskController {
+    
+    @GetMapping("/tasks")
+    public String list(Model model) {
+        var task1 = new TaskDTO(
+            1L,
+            "learn Spring Boot",
+            "make TODO application",
+            "TODO"
+        );
+        var task2 = new TaskDTO(
+            2L,
+            "learn Spring Security",
+            "add login function",
+            "TODO"
+        );
+        var taskList = List.of(task1, task2);
+        model.addAttribute("taskList", taskList);
+        return "tasks/list";
+    }
+}
